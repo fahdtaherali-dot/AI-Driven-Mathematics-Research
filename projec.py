fa230d57e023f3cbffd2130465faf8f6a9822e38
Fahd Taher Al-Shehari <fahdtaherali@gmail.com>
2026-02-01 14:51:29 +0300

    Implement user input handling and explore Python data structures (Lists, Tuples, Dicts)

--- projec.py
+++ projec.py
@@ -1,4 +1,14 @@
 aq = int(input("ادخل عمرك"))
 print("مرحبا عمرك هو", aq)
 
-result = 2 + 5
+names = ["فهد", "خالد", "طاهر", 12]
+names[1] = "عبد الله"
+names.insert(2, "امين")
+names.remove("طاهر")
+print(names)
+
+ages = [12, 45, 10, 1]
+ages.clear()
+
+child_date = ("احمد", "الرياض", "1-2-2010")
+child_date = {"name":"احمد", "city":"الرياض", "date":"1-2-2010"}
