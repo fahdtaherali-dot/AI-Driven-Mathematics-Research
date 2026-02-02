09ca2bb640e97700c30aeef7bbc372327e6975c9
Fahd Taher Al-Shehari <fahdtaherali@gmail.com>
2026-02-02 14:46:43 +0300

    Refactor: Add logic and data structures
    Converted student records to Dictionaries for better mapping. Added while and for loops to automate data processing. Organized code into modular functions for future scalability in the MathHope project.

--- projec.py
+++ projec.py
@@ -1,14 +1,40 @@
-aq = int(input("ادخل عمرك"))
-print("مرحبا عمرك هو", aq)
+def a():
+ aq = int(input("ادخل عمرك"))
 
-names = ["فهد", "خالد", "طاهر", 12]
-names[1] = "عبد الله"
-names.insert(2, "امين")
-names.remove("طاهر")
-print(names)
+ if aq <= 18:
+    print(" يمكنك الدخول")
+ elif aq <= 100:
+    print('انت شايب جدا ارحل')
+ else:
+    print("مرحبا عمرك هو", aq)
+a()
 
-ages = [12, 45, 10, 1]
-ages.clear()
+def b():
+ names = ["فهد", "خالد", "طاهر", 12]
+ names[1] = "عبد الله"
+ names.insert(2, "امين")
+ names.remove("طاهر")
+ print(names)
 
-child_date = ("احمد", "الرياض", "1-2-2010")
-child_date = {"name":"احمد", "city":"الرياض", "date":"1-2-2010"}
+ ages = [12, 45, 10, 1]
+ ages.clear()
+
+ child_date = ("احمد", "الرياض", "1-2-2010")
+ child_date = {"name":"احمد", "city":"الرياض", "date":"1-2-2010"}
+ del child_date["city"]
+ print(child_date.values())
+b()
+def c():
+ aa = 1
+ while aa <=5:
+    print(aa)
+    aa += 1
+
+ student = ['مالك', "طارق", "صهيب", "غاشم"]
+ for s in student:
+    print(s)
+
+ for w in range(5, 10):
+    print(w)
+c()
+
